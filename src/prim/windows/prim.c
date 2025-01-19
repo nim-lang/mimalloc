bff3e0ed93bda5b93d6019095e35d9fd28f3cb88
ringabout <43030857+ringabout@users.noreply.github.com>
2025-01-19 12:43:24 +0800

    patch ERROR_COMMITMENT_MINIMUM

--- src/prim/windows/prim.c
+++ src/prim/windows/prim.c
@@ -12,6 +12,9 @@ terms of the MIT license. A copy of the license can be found in the file
 #include "mimalloc/prim.h"
 #include <stdio.h>   // fputs, stderr
 
+#ifndef ERROR_COMMITMENT_MINIMUM
+#define ERROR_COMMITMENT_MINIMUM __MSABI_LONG(635)
+#endif
 
 //---------------------------------------------
 // Dynamically bind Windows API points for portability
